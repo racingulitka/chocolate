7c64c0118aa329fa2042d67203353b7bb30f21f3
racingulitka <mex83011@gmail.com>
2024-09-12 21:33:27 +0300

    minor

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,7 +3,7 @@ import styles from './ProductCard.module.scss'
 import { ProductCard as ProductCardTypings } from './ProductCard.typings'
 import Heart from './assets/Heart'
 import star from './assets/star.svg'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { useCurrency } from '@/utils/useCurrency'
 
 export default function ProductCard({
@@ -29,7 +29,7 @@ export default function ProductCard({
             className={styles.wrapper}
             onClick={() => setSelectedCard(props.id)}
         >
-            <div className={styles.topBlock} style={{ background: `url(${typeof(props.images[0]) === 'string' ? props.images[1].src : props.images[0].src})`, backgroundSize: 'contain', backgroundPosition: 'center top', backgroundRepeat: 'no-repeat' }}>
+            <div className={styles.topBlock} style={{ background: `url(${typeof(props.images[0]) === 'string' ? (props.images[1] as StaticImageData).src : props.images[0].src})`, backgroundSize: 'contain', backgroundPosition: 'center top', backgroundRepeat: 'no-repeat' }}>
                 <div className={styles.heartBlock} onClick={(e) => handleFavourite(e)}>
                     <Heart isFavourite={props.isFavourite} />
                 </div>
